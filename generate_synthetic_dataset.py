c92a8f674b79e7aa51828bb04c4ef82f50555845
Cesar Nunez <cesar23augusto16@gmail.com>
2026-01-07 23:00:44 -0600

    Added a bunch of stuff, not the right way to be committing but what the hell. Created script to generate a synthetic dataset and I split the logic of transformations between this script and the one on the dataset.

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -3,9 +3,11 @@ import cv2
 import numpy as np
 import json
 import random
+import torch
 from tqdm import tqdm
 import math
 from utils import load_config
+import albumentations as A
 
 def load_and_clean_source_data(img_dir, annotations_file):
     """
@@ -32,7 +34,9 @@ def load_and_clean_source_data(img_dir, annotations_file):
     
     for item in tqdm(image_data):
         img_path = os.path.join(img_dir, item['image_name'])
-        if not os.path.exists(img_path): continue
+        if not os.path.exists(img_path): 
+            print(f"Warning: Image {img_path} not found, skipping.")
+            continue
         
         # Load grayscale
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
@@ -82,7 +86,156 @@ def load_and_clean_source_data(img_dir, annotations_file):
     print(f"Loaded {len(stamps)} usable stamps.")
     return stamps
 
-def generate_mosaic(stamps, stamp_deck, canvas_size, min_symbols_per_image, max_symbols_per_image, max_attempts_per_symbol, overlap_tolerance, scaling_factor):
+def transform_stamp(stamp, transform_params, class_mapping):
+    """
+    Applies transformations to a stamp image and its boxes.
+    Currently a placeholder for future augmentations.
+    Args:
+        stamp: Dictionary with 'img', 'boxes', 'labels'.
+        transform_params: Parameters for transformations loaded from config.
+    Returns:
+        transformed_img: Transformed image (numpy array).
+        transformed_boxes: Transformed bounding boxes (numpy array).
+        labels: Labels (list).
+    """
+    # transformation parameters
+    transform = transform_params['transform']
+    scaling_factor = transform_params['scaling_factor']
+    target_min_size = transform_params['target_min_size']
+    target_max_size = transform_params['target_max_size']
+    # Augmentation parameters
+    augmentation_params = transform_params['augmentation_params']
+    morphological_ops = augmentation_params['morphological_ops']
+    morphological_kernels = augmentation_params['morphological_kernels']
+    # Get affine configs
+    affine_rotate = augmentation_params['affine_rotate']
+    affine_shear = augmentation_params['affine_shear']
+    affine_fill_value = augmentation_params['affine_fill_value']
+    affine_probability = augmentation_params['affine_probability']
+
+    img = stamp['img']
+    boxes = np.array(stamp['boxes'], dtype=np.float32)
+    labels_original = stamp['labels']
+    labels = torch.tensor([class_mapping[l] for l in stamp['labels']], dtype=torch.int64)
+
+    # ----------- 1. Dynamic Scaling (Min size not needed right now) ---------------
+    h, w = img.shape
+    new_w = int(w * scaling_factor)
+    new_h = int(h * scaling_factor)
+
+    # Scale down if larger than target_max_size
+    max_dim = max(new_w, new_h)
+    if max_dim > target_max_size:
+        scale_down_factor = target_max_size / max_dim
+        new_w = int(new_w * scale_down_factor)
+        new_h = int(new_h * scale_down_factor)
+    
+    # resize image
+    img = cv2.resize(img, (new_w, new_h))
+    
+    # Scale boxes: [x1, y1, x2, y2]
+    if len(boxes) > 0:
+        boxes[:, [0, 2]] *= (new_w / w)
+        boxes[:, [1, 3]] *= (new_h / h)
+
+    # ------------------------ AFFINE AND MORPHOLOGICAL TRANFORMATIONS ------------------------
+    if transform:
+        # change img to rgb for albumentations
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+        # ---------------------- 2. Affine (Rotation/Shear) -------------------------------
+        if random.random() < affine_probability:
+            # 1. Calculate Rotation-Safe Padding based on WIDTH
+            # sin(10 degrees) approx 0.17. We use 0.2 for safety.
+            current_h, current_w = img_rgb.shape[:2]
+            
+            # Add padding to Top and Bottom based on the WIDTH
+            rot_pad_h = int(current_w * 0.2) 
+            # Add a little to Left/Right just in case of shear/shift based on height
+            rot_pad_w = int(current_h * 0.2) 
+
+            top_pad = rot_pad_h // 2
+            bottom_pad = rot_pad_h - top_pad
+            left_pad = rot_pad_w // 2
+            right_pad = rot_pad_w - left_pad
+
+            # Apply the padding using OpenCV
+            img_rgb = cv2.copyMakeBorder(
+                img_rgb, 
+                top=top_pad, bottom=bottom_pad, 
+                left=left_pad, right=right_pad,
+                borderType=cv2.BORDER_CONSTANT, value=[255, 255, 255]
+            )
+
+            # Shift boxes to match new coordinates
+            if len(boxes) > 0:
+                boxes[:, 0] += left_pad
+                boxes[:, 2] += left_pad
+                boxes[:, 1] += top_pad
+                boxes[:, 3] += top_pad
+
+            # Clip boxes to image size before affine transform
+            if len(boxes) > 0:
+                h_aug, w_aug = img_rgb.shape[:2]
+                boxes[:, 0] = np.clip(boxes[:, 0], 0, w_aug)
+                boxes[:, 1] = np.clip(boxes[:, 1], 0, h_aug)
+                boxes[:, 2] = np.clip(boxes[:, 2], 0, w_aug)
+                boxes[:, 3] = np.clip(boxes[:, 3], 0, h_aug)   
+
+            
+            # Affine transformations
+            affine_transform = A.Compose([
+                A.Affine(
+                    rotate=tuple(affine_rotate),
+                    shear=tuple(affine_shear),
+                    fill=affine_fill_value,
+                    p=1.0
+                )
+            ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']))
+            # pascal voc format: [x_min, y_min, x_max, y_max]         
+            
+            try:
+                transformed = affine_transform(
+                    image=img_rgb, 
+                    bboxes=boxes.tolist(), 
+                    labels=labels.tolist()
+                )
+                img_rgb = transformed['image']
+                boxes = np.array(transformed['bboxes'], dtype=np.float32)
+
+                # Clip boxes to image size after affine transform due to possible rotation/translation
+                if len(boxes) > 0:
+                    h_aug, w_aug = img_rgb.shape[:2]
+                    boxes[:, 0] = np.clip(boxes[:, 0], 0, w_aug)
+                    boxes[:, 1] = np.clip(boxes[:, 1], 0, h_aug)
+                    boxes[:, 2] = np.clip(boxes[:, 2], 0, w_aug)
+                    boxes[:, 3] = np.clip(boxes[:, 3], 0, h_aug)  
+            except Exception as e:
+                print(f"Error in affine transformation for image: {e}")
+                pass # keep original if error occurs
+
+        # Convert back to grayscale
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+
+        # ------------------------- 3. Morphology (Thickness) -----------------------------
+        # Probability of one of two morphological ops = 0.3 * 0.5 = 0.15
+        if random.random() < 0.3:
+            morpho_kernel_size = random.choice(morphological_kernels)
+            kernel = np.ones((morpho_kernel_size, morpho_kernel_size), np.uint8)
+            op = random.choice(morphological_ops)
+            # print(f"OP: {op}, Kernel Size: {morpho_kernel_size}")
+            if op == 'dilate':
+                # Dilate image = Max filter = Expands White (Ink gets Thinner)
+                img_gray = cv2.dilate(img_gray, kernel, iterations=1)
+            elif op == 'erode':
+                # Erode image = Min filter = Expands Black (Ink gets Thicker)
+                img_gray = cv2.erode(img_gray, kernel, iterations=1)
+
+    return img_gray, boxes, labels_original
+
+
+
+def generate_mosaic(stamps, stamp_deck, canvas_size, min_symbols_per_image, max_symbols_per_image, max_attempts_per_symbol, overlap_tolerance, transform_params, class_mapping):
     """
     Places random stamps onto a blank canvas to create a synthetic image.
     Args:
@@ -92,7 +245,8 @@ def generate_mosaic(stamps, stamp_deck, canvas_size, min_symbols_per_image, max_
         min_symbols_per_image: Minimum number of symbols to place.
         max_symbols_per_image: Maximum number of symbols to place.
         max_attempts_per_symbol: Max attempts to place a symbol without overlap.
-        scaling_factor: Factor to scale each stamp.
+        overlap_tolerance: Fractional tolerance for overlap detection.
+        transform_params: Parameters for transformations loaded from config.
     Returns:
         canvas: The generated synthetic image (numpy array).
         final_boxes: List of bounding boxes for all placed symbols.
@@ -103,14 +257,10 @@ def generate_mosaic(stamps, stamp_deck, canvas_size, min_symbols_per_image, max_
     
     final_boxes = []
     final_labels = []
-    
-    # Track occupied space to avoid overlapping equations
-    # Store as [x1, y1, x2, y2] of the whole stamp
     occupied_rects = []
     
     num_items = random.randint(min_symbols_per_image, max_symbols_per_image)
-
-    count_bigger = 0
+    count_skipped = 0
 
     for _ in range(num_items):
         # Refill deck if empty
@@ -122,32 +272,24 @@ def generate_mosaic(stamps, stamp_deck, canvas_size, min_symbols_per_image, max_
         stamp_idx = stamp_deck.pop()
         stamp = stamps[stamp_idx]
         
-        stamp_boxes = np.array(stamp['boxes'])
+        transformed_img, transformed_boxes, labels = transform_stamp(stamp, transform_params, class_mapping)
         
-        if len(stamp_boxes) == 0: 
-            continue
-        
-        # Resize the stamp image
-        s_h, s_w = stamp['img'].shape
-        new_w = int(s_w * scaling_factor)
-        new_h = int(s_h * scaling_factor)
-        
+        h_stamp, w_stamp = transformed_img.shape
+
         # Don't place if it's bigger than the canvas
-        if new_w >= W or new_h >= H: 
+        if w_stamp >= W or h_stamp >= H: 
             print("Stamp too large for canvas after scaling, skipping.")
-            count_bigger += 1
+            count_skipped += 1
             continue
-        
-        resized_img = cv2.resize(stamp['img'], (new_w, new_h))
-        resized_boxes = stamp_boxes * scaling_factor
         
         # --- Random Placement with Collision Detection ---
+        placed = False
         for _ in range(max_attempts_per_symbol):
-            x_pos = random.randint(0, W - new_w)
-            y_pos = random.randint(0, H - new_h)
+            x_pos = random.randint(0, W - w_stamp)
+            y_pos = random.randint(0, H - h_stamp)
             
             # Define candidate area for the new stamp
-            candidate_rect = [x_pos, y_pos, x_pos + new_w, y_pos + new_h]
+            candidate_rect = [x_pos, y_pos, x_pos + w_stamp, y_pos + h_stamp]
             
             collision = False
             for r in occupied_rects:
@@ -175,23 +317,28 @@ def generate_mosaic(stamps, stamp_deck, canvas_size, min_symbols_per_image, max_
             
             if not collision:
                 # --- Paste the image ---
-                target_area = canvas[y_pos:y_pos+new_h, x_pos:x_pos+new_w]
-                canvas[y_pos:y_pos+new_h, x_pos:x_pos+new_w] = np.minimum(target_area, resized_img)
+                target_area = canvas[y_pos:y_pos+h_stamp, x_pos:x_pos+w_stamp]
+                canvas[y_pos:y_pos+h_stamp, x_pos:x_pos+w_stamp] = np.minimum(target_area, transformed_img)
                 
                 # Shift boxes global coordinates
-                shifted_boxes = resized_boxes.copy()
+                shifted_boxes = transformed_boxes.copy()
                 shifted_boxes[:, 0] += x_pos
                 shifted_boxes[:, 2] += x_pos
                 shifted_boxes[:, 1] += y_pos
                 shifted_boxes[:, 3] += y_pos
                 
                 final_boxes.extend(shifted_boxes.tolist())
-                final_labels.extend(stamp['labels'])
-                
+                final_labels.extend(labels)                
                 occupied_rects.append(candidate_rect)
+                placed = True
                 break
-        print(f"Skipped {count_bigger} stamps that were too big for the canvas.")
-                
+
+        if not placed: 
+            count_skipped += 1
+            print("Could not place stamp without collision, skipping and adding to deck again.")
+            # add back the stamp to the deck for future use
+            stamp_deck.append(stamp_idx)
+                    
     return canvas, final_boxes, final_labels
 
 def main():
@@ -201,6 +348,11 @@ def main():
         print(f"\tpython {os.sys.argv[0]} <path_to_config.json>")
         exit(1)
     config = load_config(os.sys.argv[1])
+
+    # Load class mapping to ensure correct IDs
+    class_mapping_path = config['paths']['class_mapping_path']
+    with open(class_mapping_path, 'r') as f:
+        class_mapping = json.load(f)
 
     # Paths to original CROHME data
     original_img_dir = config['paths']['original_image_dir']
@@ -217,7 +369,7 @@ def main():
     max_symbols_per_image = synthetic_image_params['max_symbols_per_image']
     max_attempts_per_symbol = synthetic_image_params['max_attempts_per_symbol']
     overlap_tolerance = synthetic_image_params['overlap_tolerance']
-    scaling_factor = config['transform_params']['scaling_factor']
+    transform_params = config['transform_params']
     # ---------------------
     
     os.makedirs(synthetic_image_dir, exist_ok=True)
@@ -242,7 +394,8 @@ def main():
             max_symbols_per_image=max_symbols_per_image,
             max_attempts_per_symbol=max_attempts_per_symbol,
             overlap_tolerance=overlap_tolerance,
-            scaling_factor=scaling_factor            
+            transform_params=transform_params,
+            class_mapping=class_mapping       
         )
         
         filename = f"syn_{i:05d}.png"
--- math_symbols_dataset.py
+++ math_symbols_dataset.py
@@ -21,11 +21,11 @@ class MathSymbolDataset(Dataset):
             config (dict): Configuration dictionary.
         """
         self.config = config
-        self.img_dir = config['paths']['original_image_dir']
+        self.img_dir = config['paths']['synthetic_image_dir']
 
-        # Load annotations
-        original_annotations_path = config['paths']['original_annotations_path']
-        with open(original_annotations_path, 'r') as f:
+        # Load Synthetic annotations
+        synthetic_annotations_path = config['paths']['synthetic_annotations_path']
+        with open(synthetic_annotations_path, 'r') as f:
             self.annotations = json.load(f)
 
         # Only keep the list of annotations
@@ -38,92 +38,55 @@ class MathSymbolDataset(Dataset):
         
         # Transformation parameters
         self.transform = config['transform_params']['transform']
-        self.scaling_factor = config['transform_params']['scaling_factor']
-        self.target_min_size = config['transform_params']['target_min_size']
-        self.target_max_size = config['transform_params']['target_max_size']
+
         # Augmentation parameters
         augmentation_params = config['transform_params']['augmentation_params']
-        self.morphological_ops = augmentation_params['morphological_ops']
-        self.morphological_kernels = augmentation_params['morphological_kernels']
         self.blur_kernels = augmentation_params['blur_kernels']
         self.noise_sigma_range = augmentation_params['noise_sigma_range']
         self.threshold_factor_range = augmentation_params['threshold_factor_range']
 
-        # Get affine configs
-        self.affine_rotate = augmentation_params['affine_rotate']
-        self.affine_shear = augmentation_params['affine_shear']
-        self.affine_fill_value = augmentation_params['affine_fill_value']
-        self.affine_probability = augmentation_params['affine_probability']
-
-        # Affine transformations
-        if self.transform:
-            self.affine_transform = A.Compose([
-                A.Affine(
-                    rotate=tuple(self.affine_rotate),
-                    shear=tuple(self.affine_shear),
-                    fill=self.affine_fill_value,
-                    p=1.0
-                )
-            ], bbox_params=A.BboxParams(format='pascal_voc', label_fields=['labels']))
-            # pascal voc format: [x_min, y_min, x_max, y_max]
-        else:
-            self.affine_transform = None     
-
     def __len__(self):
         return len(self.annotations)
 
-    def __mimic_whiteboard_ink(self, img):
+    def __noise_augmentation(self, img):
         """
         Applies 'analog' noise to digital ink to simulate whiteboard markers.
         Args: 
             img (numpy.ndarray): Grayscale image (H, W) with white background and black ink.
         Returns:
-            numpy.ndarray: Augmented image.
+            numpy.ndarray: Image with blur, noise and thresholding applied.
         """
-        # 1. Random Global Thickness (Marker Tip Size)
-        # Probability of one of two morphological ops = 0.3 * 0.5 = 0.15
-        if random.random() < 0.3:
-            morpho_kernel_size = random.choice(self.morphological_kernels)
-            kernel = np.ones((morpho_kernel_size, morpho_kernel_size), np.uint8)
-            op = random.choice(self.morphological_ops)
-            # print(f"OP: {op}, Kernel Size: {morpho_kernel_size}")
-            if op == 'dilate':
-                # Dilate image = Max filter = Expands White (Ink gets Thinner)
-                img = cv2.dilate(img, kernel, iterations=1)
-            elif op == 'erode':
-                # Erode image = Min filter = Expands Black (Ink gets Thicker)
-                img = cv2.erode(img, kernel, iterations=1)
 
-        # 2. Blur + Noise + Threshold
-        if random.random() < 0.8:
-            # A. Blur to create gray transition areas
-            blur_amount = random.choice(self.blur_kernels)
-            img_blurred = cv2.GaussianBlur(img, (blur_amount, blur_amount), 0)
-            #print(f"Blur Amount: {blur_amount}")
-            
-            # B. Add Gaussian Noise            
-            noise_sigma = random.randint(self.noise_sigma_range[0], self.noise_sigma_range[1])
-            noise = np.random.normal(0, noise_sigma, img_blurred.shape).astype(np.int16)
-            img_noisy = img_blurred.astype(np.int16) + noise
-            img_noisy = np.clip(img_noisy, 0, 255).astype(np.uint8)
-
-            # C. Adaptive thresholding based on dynamic range
-            min_val = np.min(img_noisy) # Darkest pixel (The Ink)
-            max_val = np.max(img_noisy) # Brightest pixel (The Background)
-            dynamic_range = max_val - min_val
-            
-            if dynamic_range < 20:
-                return img
+        if self.transform:
+            # 2. Blur + Noise + Threshold
+            if random.random() < 0.8:
+                # A. Blur to create gray transition areas
+                blur_amount = random.choice(self.blur_kernels)
+                img_blurred = cv2.GaussianBlur(img, (blur_amount, blur_amount), 0)
+                #print(f"Blur Amount: {blur_amount}")
                 
-            # The threshold value is set to a random factor of the dynamic range above the min_val
-            thresh_factor = random.uniform(self.threshold_factor_range[0], self.threshold_factor_range[1]) 
-            threshold_val = (dynamic_range * thresh_factor) + min_val
+                # B. Add Gaussian Noise            
+                noise_sigma = random.randint(self.noise_sigma_range[0], self.noise_sigma_range[1])
+                noise = np.random.normal(0, noise_sigma, img_blurred.shape).astype(np.int16)
+                img_noisy = img_blurred.astype(np.int16) + noise
+                img_noisy = np.clip(img_noisy, 0, 255).astype(np.uint8)
+                img = img_noisy
+
+        # C. Adaptive thresholding based on dynamic range
+        min_val = np.min(img) # Darkest pixel (The Ink)
+        max_val = np.max(img) # Brightest pixel (The Background)
+        dynamic_range = max_val - min_val
             
-            _, img_result = cv2.threshold(img_noisy, int(threshold_val), 255, cv2.THRESH_BINARY)
-            # print(f"Noise Sigma: {noise_sigma}, Threshold Value: {threshold_val:.2f}, Thresh Factor: {thresh_factor:.2f}")
-            return img_result
+        if dynamic_range < 20:
+            return img
+                
+        # The threshold value is set to a random factor of the dynamic range above the min_val
+        thresh_factor = random.uniform(self.threshold_factor_range[0], self.threshold_factor_range[1]) 
+        threshold_val = (dynamic_range * thresh_factor) + min_val
         
-        return img
+        _, img_result = cv2.threshold(img, int(threshold_val), 255, cv2.THRESH_BINARY)
+        # print(f"Noise Sigma: {noise_sigma}, Threshold Value: {threshold_val:.2f}, Thresh Factor: {thresh_factor:.2f}")
+        return img_result
 
     def __getitem__(self, idx):
         # 1. Load Data
@@ -144,125 +107,8 @@ class MathSymbolDataset(Dataset):
         boxes = np.array(item['boxes'], dtype=np.float32)
         labels = torch.tensor([self.class_mapping[l] for l in item['labels']], dtype=torch.int64)
 
-        # 2. Dynamic Scaling
-        # Apply scaling factor to match whiteboard symbol sizes
-        h, w = img_gray.shape
-        new_w = int(w * self.scaling_factor)
-        new_h = int(h * self.scaling_factor)
-
-        # Scale down if larger than target_max_size
-        max_dim = max(new_w, new_h)
-        if max_dim > self.target_max_size:
-            scale_down_factor = self.target_max_size / max_dim
-            new_w = int(new_w * scale_down_factor)
-            new_h = int(new_h * scale_down_factor)
-        
-        # resize image
-        img_gray = cv2.resize(img_gray, (new_w, new_h))
-        
-        # Scale boxes: [x1, y1, x2, y2]
-        if len(boxes) > 0:
-            boxes[:, [0, 2]] *= (new_w / w)
-            boxes[:, [1, 3]] *= (new_h / h)
-
-        # 3. Padding
-        # Pad with white pixels if smaller than target_min_size
-        # Faster R-CNN needs a minimum size to generate feature maps
-        pad_total_w = max(0, self.target_min_size - new_w) # if negative, pad_w = 0, no padding needed / else pad_w = amount to pad
-        pad_total_h = max(0, self.target_min_size - new_h)
-        
-        if pad_total_w > 0 or pad_total_h > 0:
-            # Split padding to center the image
-            pad_top = pad_total_h // 2
-            pad_bottom = pad_total_h - pad_top
-            pad_left = pad_total_w // 2
-            pad_right = pad_total_w - pad_left
-
-            # Apply Padding
-            img_gray = cv2.copyMakeBorder(
-                img_gray, 
-                top=pad_top, bottom=pad_bottom, 
-                left=pad_left, right=pad_right, 
-                borderType=cv2.BORDER_CONSTANT, value=(255, 255, 255)
-            )
-            
-            # Shift boxes to match the new image position
-            if len(boxes) > 0:
-                boxes[:, 0] += pad_left
-                boxes[:, 2] += pad_left
-                boxes[:, 1] += pad_top
-                boxes[:, 3] += pad_top
-
-        # 4. Domain Augmentation (Morphological + Noise + Affine)
-        # Change ink thickness sometimes + blur + noise + threshold + affine transformations
-        if self.transform:
-            # Change image to rgb for albumentations
-            img_rgb = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
-
-            if random.random() < self.affine_probability:
-                #print("Applying Affine Transformation...")
-                # 1. Calculate Rotation-Safe Padding based on WIDTH
-                # sin(10 degrees) approx 0.17. We use 0.2 for safety.
-                current_h, current_w = img_rgb.shape[:2]
-                
-                # Add padding to Top and Bottom based on the WIDTH
-                rot_pad_h = int(current_w * 0.2) 
-                # Add a little to Left/Right just in case of shear/shift based on height
-                rot_pad_w = int(current_h * 0.2) 
-
-                top_pad = rot_pad_h // 2
-                bottom_pad = rot_pad_h - top_pad
-                left_pad = rot_pad_w // 2
-                right_pad = rot_pad_w - left_pad
-
-                # Apply the padding using OpenCV
-                img_rgb = cv2.copyMakeBorder(
-                    img_rgb, 
-                    top=top_pad, bottom=bottom_pad, 
-                    left=left_pad, right=right_pad,
-                    borderType=cv2.BORDER_CONSTANT, value=[255, 255, 255]
-                )
-
-                # Shift boxes to match new coordinates
-                if len(boxes) > 0:
-                    boxes[:, 0] += left_pad
-                    boxes[:, 2] += left_pad
-                    boxes[:, 1] += top_pad
-                    boxes[:, 3] += top_pad
-
-                # Clip boxes to image size before affine transform
-                if len(boxes) > 0:
-                    h_aug, w_aug = img_rgb.shape[:2]
-                    boxes[:, 0] = np.clip(boxes[:, 0], 0, w_aug)
-                    boxes[:, 1] = np.clip(boxes[:, 1], 0, h_aug)
-                    boxes[:, 2] = np.clip(boxes[:, 2], 0, w_aug)
-                    boxes[:, 3] = np.clip(boxes[:, 3], 0, h_aug)            
-                
-                try:
-                    transformed = self.affine_transform(
-                        image=img_rgb, 
-                        bboxes=boxes.tolist(), 
-                        labels=labels.tolist()
-                    )
-                    img_rgb = transformed['image']
-                    boxes = np.array(transformed['bboxes'], dtype=np.float32)
-                    labels = torch.tensor(transformed['labels'], dtype=torch.int64)
-
-                    # Clip boxes to image size after affine transform due to possible rotation/translation
-                    if len(boxes) > 0:
-                        h_aug, w_aug = img_rgb.shape[:2]
-                        boxes[:, 0] = np.clip(boxes[:, 0], 0, w_aug)
-                        boxes[:, 1] = np.clip(boxes[:, 1], 0, h_aug)
-                        boxes[:, 2] = np.clip(boxes[:, 2], 0, w_aug)
-                        boxes[:, 3] = np.clip(boxes[:, 3], 0, h_aug)  
-                except Exception as e:
-                    print(f"Error in affine transformation for image {img_path}: {e}")
-                    pass # keep original if error occurs
-
-            # Convert back to grayscale
-            img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-            # Apply whiteboard ink mimicry
-            img_gray = self.__mimic_whiteboard_ink(img_gray)
+        # Apply Global Noise + Blur + Threshold Augmentation
+        img_gray = self.__noise_augmentation(img_gray)
 
         # Convert to RGB for model input (faster rcnn expects 3 channels)
         img = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
